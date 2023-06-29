64a3ff828483173d2587c126ea617db49c1b5143
Wanglidi <lidiwang@sjtu.edu.cn>
2023-06-29 23:03:08 +0800

    feat: Adding function generating the main branch of the tentacles of the enemy (need to be fixed)

--- src/Scenes/Level/Enemy/Random.elm
+++ src/Scenes/Level/Enemy/Random.elm
@@ -1,6 +1,10 @@
 module Scenes.Level.Enemy.Random exposing (..)
 
 import Random
+import Scenes.Level.Frame.Functions exposing (cellLength)
+import Canvas exposing (Point)
+import List
+import Tuple
 
 {-| control the random seed in Enemy Layer-}
 randomEnemy : Random.Seed -> (Int, Random.Seed)
@@ -10,3 +14,62 @@ randomEnemy seed =
             Random.int 0 1000
     in
     Random.step number seed
+
+
+{- The following five functions are coefficients for curUniqueSin -}
+partialCoefficient : Float
+partialCoefficient = 
+    0.45
+
+period1 : Float
+period1 =
+    20
+
+period2 : Float 
+period2 =
+    30
+
+period3 : Float
+period3 =
+    36
+
+xNum : Int
+xNum =
+    6
+
+{-| Get a function which change continuously for main branch of a tentacle -}
+{-
+Explicit Function:
+y = Asin(kx+d),
+in which
+    A = 0.5+0.5C1,
+    d = 2/3*2*PI/B*C2,
+    k = (1/3 + 2/3*C3) * 2*PI/B,
+        B = partialC * cellLength,
+        Ci = (q % pi) / pi (i = 1,2,3),
+        p1,2,3 are determined coefficients for A,d,k,
+        q = (pos + id + t).
+-}
+curUniqueSin : Int -> Point -> Int -> List Point
+curUniqueSin time pos id =
+    let
+        b = partialCoefficient * cellLength
+        q = Tuple.first pos + Tuple.second pos + toFloat (id + time)
+        c1 = ( toFloat ( modBy (round q) (round period1) ) ) / period1
+        c2 = ( toFloat ( modBy (round q) (round period2) ) ) / period2
+        c3 = ( toFloat ( modBy (round q) (round period3) ) ) / period3
+        a = 5+5*c1
+        d = 2/3*2*pi/b*c2
+        k = (1/3 + 2/3*c3) * 2*pi/b
+        x = List.map (listModifyNum (b/ (toFloat xNum))) (List.range 1 xNum)
+    in
+    List.map (generatePairSin a d k) x
+
+{- The following two functions are used for map -}
+generatePairSin : Float -> Float -> Float -> Float -> Point
+generatePairSin a d k x =
+    (x, a*sin ( radians (k*x + d)))
+
+listModifyNum : Float -> Int -> Float
+listModifyNum div x =
+    (toFloat x) * div
--- src/Scenes/Level/Enemy/Render.elm
+++ src/Scenes/Level/Enemy/Render.elm
@@ -6,9 +6,11 @@ import Canvas.Settings.Text exposing (TextAlign(..), align, font)
 import Lib.Layer.Base exposing (LayerMsg(..), LayerTarget(..))
 import Scenes.Level.Enemy.Common exposing (EnvC, Model, nullModel, initEnemy1, EnemyState(..), EnemyBlock, Cell)
 import Scenes.Level.SceneInit exposing (LevelInit)
-import Scenes.Level.Frame.Functions exposing (coorChange, point2Int, upperCell, leftCell, rightCell, lowerCell, cellLength)
-import Scenes.Level.Frame.Functions exposing (lengthChange)
+import Scenes.Level.Frame.Functions exposing (coorChange, lengthChange, point2Int, upperCell, leftCell, rightCell, lowerCell, cellLength)
+import Scenes.Level.Enemy.Random exposing (curUniqueSin)
 import List
+import Tuple
+import Color exposing (Color)
 
 {-| render the whole enemy body(explicitly the body field in model) -}
 renderEnemyBody : EnvC -> Model -> Renderable
@@ -32,27 +34,51 @@ renderEnemyBlock env model x =
         color =
             tmp_enemyblock.color
     in
-    renderEnemyBlockCentral env x 
+    renderEnemyBlockCentral env model x 
 
 {-| Render the central part of an enemy block,
     which is a circle exists as long as there is a block. -}
-renderEnemyBlockCentral : EnvC -> Cell EnemyBlock -> Renderable
-renderEnemyBlockCentral env x =
+renderEnemyBlockCentral : EnvC -> Model -> Cell EnemyBlock -> Renderable
+renderEnemyBlockCentral env model x =
     let
         color =
             x.val.color
         pos = 
             x.pos
     in
-    shapes [ fill color ] [ circle (coorChange env pos) (lengthChange env (cellLength/2.5)) ]
+    Canvas.group
+    []
+    [   shapes [ fill color ] [ circle (coorChange env pos) (lengthChange env (cellLength/2.5)) ]
+    ,   renderUpperTentacle env model x
+    ]
 
-{-| generic function of rendering number(Int) for testing -}
-renderNum : EnvC -> Int -> Renderable
-renderNum env num =
-    text [ font { size = 24, family = "Arial", style = "" }, align Center ] ( 50, 50 ) ("remained bricks:" ++ String.fromInt num)
+renderUpperTentacle : EnvC -> Model -> Cell EnemyBlock -> Renderable
+renderUpperTentacle env model x =
+    let
+        color =
+            x.val.color
+        pos = 
+            x.pos
+        id = 1
+        sinPair =
+            curUniqueSin model.time pos id
+        sinPair2 =
+            List.map (offsetPoint (offsetPoint (0.0, 50.0) pos)) sinPair
+    in
+    Canvas.group [] (List.map (renderCircleMap1 env color 2.6) sinPair2)
+
+
+{-| rendering circle for map function -}
+renderCircleMap1 : EnvC -> Color -> Float -> Point -> Renderable
+renderCircleMap1 env color radius pos =
+    shapes [ fill color ] [ circle (coorChange env pos) (lengthChange env radius) ]
+
+{-| adding offset to List Point for map function -}
+offsetPoint : Point -> Point -> Point
+offsetPoint pos offset =
+    (Tuple.first pos + Tuple.first offset, Tuple.second pos + Tuple.second offset)
 
 {-
-renderUpperTentacle : EnvC -> Cell EnemyBlock -> Renderable
 renderRightTentacle : EnvC -> Cell EnemyBlock -> Renderable
 renderLowerTentacle : EnvC -> Cell EnemyBlock -> Renderable
 renderLeftTentacle : EnvC -> Cell EnemyBlock -> Renderable
@@ -62,3 +88,13 @@ renderRightConnection : EnvC -> Cell EnemyBlock -> Renderable
 renderLowerConnection : EnvC -> Cell EnemyBlock -> Renderable
 renderLeftConnection : EnvC -> Cell EnemyBlock -> Renderable
 -}
+
+{-| generic function of rendering number(Int) for testing -}
+renderNum : EnvC -> Int -> Renderable
+renderNum env num =
+    text [ font { size = 24, family = "Arial", style = "" }, align Center ] (coorChange env ( 50, 50 )) ("remained bricks:" ++ String.fromInt num)
+
+{-| generic function of rendering circle -}
+renderCircle : EnvC -> Point -> Int -> Color -> Renderable
+renderCircle env pos radius color =
+    shapes [ fill color ] [ circle (coorChange env pos) (lengthChange env (toFloat radius)) ]
