9612a8f810362b4733c5a56521d4bb2d08e5106f
Wanglidi <lidiwang@sjtu.edu.cn>
2023-07-05 21:05:22 +0800

    fix: fit in elm format

--- src/Scenes/Level/Enemy/Common.elm
+++ src/Scenes/Level/Enemy/Common.elm
@@ -1,4 +1,7 @@
-module Scenes.Level.Enemy.Common exposing (Model, nullModel, EnvC, EnemyState(..), initEnemy1, EnemyBlock, Cell, EnemyCore, GridLoc, maxEyeV)
+module Scenes.Level.Enemy.Common exposing
+    ( Model, nullModel, EnvC
+    , Cell, EnemyBlock, EnemyCore, EnemyState(..), GridLoc, initEnemy1, maxEyeV
+    )
 
 {-| Common module
 
@@ -9,134 +12,139 @@ module Scenes.Level.Enemy.Common exposing (Model, nullModel, EnvC, EnemyState(..
 import Canvas exposing (Point)
 import Color exposing (Color)
 import Lib.Env.Env as Env
-import Scenes.Level.LayerBase exposing (CommonData)
-import Scenes.Level.Frame.Functions exposing (coorChange, point2Int, int2Point, cellLength)
 import Random
 import Scenes.Level.Enemy.Random exposing (randomEnemy)
+import Scenes.Level.Frame.Functions exposing (cellLength, coorChange, int2Point, point2Int)
+import Scenes.Level.LayerBase exposing (CommonData)
 import Time exposing (Posix, now)
 
 
 {-| Model
 Add your own data here.
 -}
-
 type EnemyState
-    =   Alive
-    |   Stopped
-    |   Dead
+    = Alive
+    | Stopped
+    | Dead
+
 
 type alias GridLoc =
     ( Int, Int )
 
+
 type alias Cell a =
-    {
-        val : a
-    ,   loc : GridLoc
+    { val : a
+    , loc : GridLoc
     }
+
 
 type alias Grid a =
     List (Cell a)
 
-type alias EnemyCore = 
-    {
-        color : Color
-    ,   hp : Int
+
+type alias EnemyCore =
+    { color : Color
+    , hp : Int
     }
+
 
 type alias EnemyBlock =
-    {
-        color : Color
-    ,   hp : Int
+    { color : Color
+    , hp : Int
     }
+
 
 type alias EnemyEye =
-    {
-        pos : Point
-    ,   v : Point
-    ,   target : Point
-    ,   target_eroded : Bool
-    ,   target_loc : GridLoc
+    { pos : Point
+    , v : Point
+    , target : Point
+    , target_eroded : Bool
+    , target_loc : GridLoc
     }
 
+
 type alias Model =
-    {
-        status : EnemyState
-    ,   body : Grid EnemyBlock
-    ,   core : Cell EnemyCore
-    ,   map_size : GridLoc
-    ,   seed : Random.Seed
-    ,   randNum : Int
-    ,   time : Int
-    ,   target : GridLoc
-    ,   eye : EnemyEye
+    { status : EnemyState
+    , body : Grid EnemyBlock
+    , core : Cell EnemyCore
+    , map_size : GridLoc
+    , seed : Random.Seed
+    , randNum : Int
+    , time : Int
+    , target : GridLoc
+    , eye : EnemyEye
     }
 
 
 {-| nullModel
 -}
-
 nullEnemyCore : Cell EnemyCore
 nullEnemyCore =
-    {
-        val = { color = Color.red
-            ,   hp = 1
-            }
-    ,   loc = ( 3, 1 )
+    { val =
+        { color = Color.red
+        , hp = 1
+        }
+    , loc = ( 3, 1 )
     }
+
 
 nullEnemyEye : EnemyEye
 nullEnemyEye =
-    {
-        pos = ( 350, 150 )
-    ,   v = ( 0, 0 )
-    ,   target = ( 350, 150 )
-    ,   target_eroded = True
-    ,   target_loc = ( 3, 1 )
+    { pos = ( 350, 150 )
+    , v = ( 0, 0 )
+    , target = ( 350, 150 )
+    , target_eroded = True
+    , target_loc = ( 3, 1 )
     }
+
 
 maxEyeV : Float
 maxEyeV =
     5
 
+
 nullModel : Model
 nullModel =
     let
-        (number, seed) =
+        ( number, seed ) =
             randomEnemy (Random.initialSeed 0)
     in
-    {
-        status = Stopped
-    ,   body = []
-    ,   core = nullEnemyCore
-    ,   map_size = ( 0, 0 )
-    ,   seed = seed
-    ,   randNum = number
-    ,   time = 0
-    ,   target = ( -1, -1 )
-    ,   eye = nullEnemyEye
+    { status = Stopped
+    , body = []
+    , core = nullEnemyCore
+    , map_size = ( 0, 0 )
+    , seed = seed
+    , randNum = number
+    , time = 0
+    , target = ( -1, -1 )
+    , eye = nullEnemyEye
     }
+
 
 initEnemy1 : Model
 initEnemy1 =
     let
-        (number, seed) =
+        ( number, seed ) =
             randomEnemy (Random.initialSeed 0)
     in
-    {
-        status = Dead
-    ,   body = [    {   val = { color = Color.black
-                            ,   hp = 1}
-                    ,   loc = ( 3, 1 )
-                        }
-                ]
-    ,   core = nullEnemyCore
-    ,   map_size = ( 4, 3 )
-    ,   seed = seed
-    ,   randNum = number
-    ,   time = 0
-    ,   target = ( -1, -1 )
-    ,   eye = nullEnemyEye
+    { status = Dead
+    , body =
+        [ { val =
+                { color = Color.black
+                , hp = 1
+                }
+          , loc = ( 3, 1 )
+          }
+        ]
+    , core = nullEnemyCore
+    , map_size = ( 4, 3 )
+    , seed = seed
+    , randNum = number
+    , time = 0
+    , target = ( -1, -1 )
+    , eye = nullEnemyEye
     }
+
 
 {-| Convenient type alias for the environment
 -}
--- src/Scenes/Level/Enemy/Render.elm
+++ src/Scenes/Level/Enemy/Render.elm
@@ -1,25 +1,22 @@
 module Scenes.Level.Enemy.Render exposing (..)
 
-import Canvas exposing (Renderable, empty, Point, group, shapes, circle, rect, text)
+import Canvas exposing (Point, Renderable, circle, empty, group, rect, shapes, text)
 import Canvas.Settings exposing (fill)
-import Canvas.Settings.Advanced exposing (transform, rotate, translate)
+import Canvas.Settings.Advanced exposing (rotate, transform, translate)
 import Canvas.Settings.Text exposing (TextAlign(..), align, font)
-import Lib.Layer.Base exposing (LayerMsg(..), LayerTarget(..))
-import Scenes.Level.Enemy.Common exposing (EnvC, Model, nullModel, initEnemy1, EnemyState(..), EnemyBlock, Cell)
-import Scenes.Level.SceneInit exposing (LevelInit)
-import Scenes.Level.Frame.Functions exposing (coorChange, lengthChange, point2Int, upperCell, leftCell, rightCell, lowerCell, cellLength, addPoint, grid2real)
-import Scenes.Level.Enemy.Random exposing (curUniqueSin)
-import List
-import Tuple
 import Color exposing (Color)
-import Scenes.Level.Frame.Functions exposing (int2Point)
-import Scenes.Level.Enemy.Common exposing (GridLoc)
+import Lib.Layer.Base exposing (LayerMsg(..), LayerTarget(..))
+import List
+import Scenes.Level.Enemy.Common exposing (Cell, EnemyBlock, EnemyCore, EnemyState(..), EnvC, GridLoc, Model, initEnemy1, nullModel)
+import Scenes.Level.Enemy.Random exposing (curUniqueSin)
 import Scenes.Level.Enemy.Update exposing (checkCellLoc)
-import Scenes.Level.Enemy.Common exposing (EnemyCore)
-import Canvas.Settings.Advanced exposing (rotate)
-import Scenes.Level.Frame.Functions exposing (scalePointLength)
+import Scenes.Level.Frame.Functions exposing (addPoint, cellLength, coorChange, grid2real, int2Point, leftCell, lengthChange, lowerCell, point2Int, rightCell, scalePointLength, upperCell)
+import Scenes.Level.SceneInit exposing (LevelInit)
+import Tuple
 
-{-| render the whole enemy body(explicitly the body field in model) -}
+
+{-| render the whole enemy body(explicitly the body field in model)
+-}
 renderEnemyBody : EnvC -> Model -> Renderable
 renderEnemyBody env model =
     let
@@ -27,230 +24,368 @@ renderEnemyBody env model =
             List.map (renderEnemyBlock env model) model.body
     in
     Canvas.group
-    []
-    rend
+        []
+        rend
 
-{-| render a single enemy block -}
+
+{-| render a single enemy block
+-}
 renderEnemyBlock : EnvC -> Model -> Cell EnemyBlock -> Renderable
 renderEnemyBlock env model x =
     let
         loc =
             x.loc
+
         tmp_enemyblock =
             x.val
+
         tentacle_render =
             Canvas.group [] (List.map (renderSingleTentacle env model x) (tentacleDir model loc))
     in
     Canvas.group
-    []
-    [   renderEnemyBlockCentral env model x 
-    ,   tentacle_render
-    ]
+        []
+        [ renderEnemyBlockCentral env model x
+        , tentacle_render
+        ]
 
-{-| generate a list representing the available tentacles direction (0 to 11) -}
+
+{-| generate a list representing the available tentacles direction (0 to 11)
+-}
 tentacleDir : Model -> GridLoc -> List Int
 tentacleDir model loc =
     let
-        (x, y) = loc
-        right = case (List.any (checkCellLoc (x+1, y)) model.body) of
-                    True -> []
-                    False -> [0,1,2]
-        up =    case (List.any (checkCellLoc (x, y-1)) model.body) of
-                    True -> []
-                    False -> [3,4,5]
-        left =  case (List.any (checkCellLoc (x-1, y)) model.body) of
-                    True -> []
-                    False -> [6,7,8]
-        down =  case (List.any (checkCellLoc (x, y+1)) model.body) of
-                    True -> []
-                    False -> [9,10,11]
+        ( x, y ) =
+            loc
+
+        right =
+            case List.any (checkCellLoc ( x + 1, y )) model.body of
+                True ->
+                    []
+
+                False ->
+                    [ 0, 1, 2 ]
+
+        up =
+            case List.any (checkCellLoc ( x, y - 1 )) model.body of
+                True ->
+                    []
+
+                False ->
+                    [ 3, 4, 5 ]
+
+        left =
+            case List.any (checkCellLoc ( x - 1, y )) model.body of
+                True ->
+                    []
+
+                False ->
+                    [ 6, 7, 8 ]
+
+        down =
+            case List.any (checkCellLoc ( x, y + 1 )) model.body of
+                True ->
+                    []
+
+                False ->
+                    [ 9, 10, 11 ]
     in
-    List.concat [right, up, left, down]
+    List.concat [ right, up, left, down ]
+
 
 {-| Render the central part of an enemy block,
-    which is a circle exists as long as there is a block. -}
+which is a circle exists as long as there is a block.
+-}
 renderEnemyBlockCentral : EnvC -> Model -> Cell EnemyBlock -> Renderable
 renderEnemyBlockCentral env model x =
     let
         color =
             x.val.color
-        loc = 
+
+        loc =
             x.loc
     in
     Canvas.group
-    []
-    [   shapes [ fill color ] [ rect (coorChange env (grid2real loc)) (lengthChange env (cellLength)) (lengthChange env (cellLength)) ]
-    ]
+        []
+        [ shapes [ fill color ] [ rect (coorChange env (grid2real loc)) (lengthChange env cellLength) (lengthChange env cellLength) ]
+        ]
 
-{-| 
-rendering a single tentacle
-tentacle id |   direction
-0           |   right 1
-1           |   right 2
-2           |   right 3
-3           |   up 1
-4           |   up 2
-5           |   up 3
-6           |   left 1
-7           |   left 2
-8           |   left 3
-9           |   down 1
-10          |   down 2
-11          |   down 3
- -}
+
+{-| rendering a single tentacle
+tentacle id | direction
+0 | right 1
+1 | right 2
+2 | right 3
+3 | up 1
+4 | up 2
+5 | up 3
+6 | left 1
+7 | left 2
+8 | left 3
+9 | down 1
+10 | down 2
+11 | down 3
+-}
 renderSingleTentacle : EnvC -> Model -> Cell EnemyBlock -> Int -> Renderable
 renderSingleTentacle env model x id =
     let
         color =
             x.val.color
-        (locx, locy) = 
+
+        ( locx, locy ) =
             x.loc
+
         sinPair =
             curUniqueSin model.time x.loc id
-        (rotate, offset) =  case id of
-                                0 -> (0, ( (toFloat locx + 0.7)*cellLength, ( (toFloat locy) + (toFloat(id)/10)*3 + 0.2 )*cellLength))
-                                1 -> (0, ( (toFloat locx + 0.7)*cellLength, ( (toFloat locy) + (toFloat(id)/10)*3 + 0.2 )*cellLength))
-                                2 -> (0, ( (toFloat locx + 0.7)*cellLength, ( (toFloat locy) + (toFloat(id)/10)*3 + 0.2 )*cellLength))
-                                3 -> (1, ( ( (toFloat locx) + (toFloat(id-3)/10)*3 + 0.2 )*cellLength, (toFloat locy + 0.2)*cellLength))
-                                4 -> (1, ( ( (toFloat locx) + (toFloat(id-3)/10)*3 + 0.2 )*cellLength, (toFloat locy + 0.2)*cellLength))
-                                5 -> (1, ( ( (toFloat locx) + (toFloat(id-3)/10)*3 + 0.2 )*cellLength, (toFloat locy + 0.2)*cellLength))
-                                6 -> (2, ( (toFloat locx + 0.3)*cellLength, ( (toFloat locy) + (toFloat(id-6)/10)*3 + 0.2 )*cellLength))
-                                7 -> (2, ( (toFloat locx + 0.3)*cellLength, ( (toFloat locy) + (toFloat(id-6)/10)*3 + 0.2 )*cellLength))
-                                8 -> (2, ( (toFloat locx + 0.3)*cellLength, ( (toFloat locy) + (toFloat(id-6)/10)*3 + 0.2 )*cellLength))
-                                9 -> (3, ( ( (toFloat locx) + (toFloat(id-9)/10)*3 + 0.2 )*cellLength, (toFloat locy + 0.8)*cellLength))
-                                10 -> (3, ( ( (toFloat locx) + (toFloat(id-9)/10)*3 + 0.2 )*cellLength, (toFloat locy + 0.8)*cellLength))
-                                11 -> (3, ( ( (toFloat locx) + (toFloat(id-9)/10)*3 + 0.2 )*cellLength, (toFloat locy + 0.8)*cellLength))
-                                _ -> (0, ( (toFloat locx + 0.7)*cellLength, ( (toFloat locy) + (toFloat(id)/10)*3 )*cellLength))
+
+        ( rotate, offset ) =
+            case id of
+                0 ->
+                    ( 0, ( (toFloat locx + 0.7) * cellLength, (toFloat locy + (toFloat id / 10) * 3 + 0.2) * cellLength ) )
+
+                1 ->
+                    ( 0, ( (toFloat locx + 0.7) * cellLength, (toFloat locy + (toFloat id / 10) * 3 + 0.2) * cellLength ) )
+
+                2 ->
+                    ( 0, ( (toFloat locx + 0.7) * cellLength, (toFloat locy + (toFloat id / 10) * 3 + 0.2) * cellLength ) )
+
+                3 ->
+                    ( 1, ( (toFloat locx + (toFloat (id - 3) / 10) * 3 + 0.2) * cellLength, (toFloat locy + 0.2) * cellLength ) )
+
+                4 ->
+                    ( 1, ( (toFloat locx + (toFloat (id - 3) / 10) * 3 + 0.2) * cellLength, (toFloat locy + 0.2) * cellLength ) )
+
+                5 ->
+                    ( 1, ( (toFloat locx + (toFloat (id - 3) / 10) * 3 + 0.2) * cellLength, (toFloat locy + 0.2) * cellLength ) )
+
+                6 ->
+                    ( 2, ( (toFloat locx + 0.3) * cellLength, (toFloat locy + (toFloat (id - 6) / 10) * 3 + 0.2) * cellLength ) )
+
+                7 ->
+                    ( 2, ( (toFloat locx + 0.3) * cellLength, (toFloat locy + (toFloat (id - 6) / 10) * 3 + 0.2) * cellLength ) )
+
+                8 ->
+                    ( 2, ( (toFloat locx + 0.3) * cellLength, (toFloat locy + (toFloat (id - 6) / 10) * 3 + 0.2) * cellLength ) )
+
+                9 ->
+                    ( 3, ( (toFloat locx + (toFloat (id - 9) / 10) * 3 + 0.2) * cellLength, (toFloat locy + 0.8) * cellLength ) )
+
+                10 ->
+                    ( 3, ( (toFloat locx + (toFloat (id - 9) / 10) * 3 + 0.2) * cellLength, (toFloat locy + 0.8) * cellLength ) )
+
+                11 ->
+                    ( 3, ( (toFloat locx + (toFloat (id - 9) / 10) * 3 + 0.2) * cellLength, (toFloat locy + 0.8) * cellLength ) )
+
+                _ ->
+                    ( 0, ( (toFloat locx + 0.7) * cellLength, (toFloat locy + (toFloat id / 10) * 3) * cellLength ) )
+
         rend =
-            List.map (renderTentaclePixels env model color (rotate, offset) ) sinPair
+            List.map (renderTentaclePixels env model color ( rotate, offset )) sinPair
     in
     Canvas.group
-    []
-    rend    
+        []
+        rend
+
 
 isOdd : Int -> Int
 isOdd x =
     let
         str =
             String.fromInt x
+
         strr =
             String.right 1 str
     in
     case strr of
-        "1" -> 1
-        "3" -> 3
-        "5" -> 5
-        "7" -> 7
-        "9" -> 9
-        _ -> 0
+        "1" ->
+            1
+
+        "3" ->
+            3
+
+        "5" ->
+            5
+
+        "7" ->
+            7
+
+        "9" ->
+            9
+
+        _ ->
+            0
 
 
-renderTentaclePixels : EnvC -> Model -> Color -> (Int, Point) -> Point -> Renderable
-renderTentaclePixels env model color (rotate, offset) pos =
+renderTentaclePixels : EnvC -> Model -> Color -> ( Int, Point ) -> Point -> Renderable
+renderTentaclePixels env model color ( rotate, offset ) pos =
     let
         x =
             Tuple.first pos
+
         y =
             Tuple.second pos
+
         flag0 =
             model.randNum + round y
-        delta = 0
-        flag1 = isOdd flag0
-        flag2 = isOdd (flag0//2)
-        flag3 = isOdd (flag0//4)
-        flag4 = isOdd (flag0//8)
-        l1 = isOdd ((model.randNum + (round y))//2) +1
-        l2 = isOdd ((model.randNum + (round y))//3) +1
-        l3 = isOdd ((model.randNum + (round y))//4) +1
-        l4 = isOdd ((model.randNum + (round y))//5) +1
-        l5 = isOdd ((model.randNum + (round y))//6) +1
-        l6 = isOdd ((model.randNum + (round y))//7) +1
-        l7 = isOdd ((model.randNum + (round y))//8) +1
-        l8 = isOdd ((model.randNum + (round y))//9) +1
-        offset1 = int2Point (-l5, l8)
-        offset2 = int2Point (l6, -l7)
-        offset3 = int2Point (-l7, l6)
-        offset4 = int2Point (l8, -l5)
+
+        delta =
+            0
+
+        flag1 =
+            isOdd flag0
+
+        flag2 =
+            isOdd (flag0 // 2)
+
+        flag3 =
+            isOdd (flag0 // 4)
+
+        flag4 =
+            isOdd (flag0 // 8)
+
+        l1 =
+            isOdd ((model.randNum + round y) // 2) + 1
+
+        l2 =
+            isOdd ((model.randNum + round y) // 3) + 1
+
+        l3 =
+            isOdd ((model.randNum + round y) // 4) + 1
+
+        l4 =
+            isOdd ((model.randNum + round y) // 5) + 1
+
+        l5 =
+            isOdd ((model.randNum + round y) // 6) + 1
+
+        l6 =
+            isOdd ((model.randNum + round y) // 7) + 1
+
+        l7 =
+            isOdd ((model.randNum + round y) // 8) + 1
+
+        l8 =
+            isOdd ((model.randNum + round y) // 9) + 1
+
+        offset1 =
+            int2Point ( -l5, l8 )
+
+        offset2 =
+            int2Point ( l6, -l7 )
+
+        offset3 =
+            int2Point ( -l7, l6 )
+
+        offset4 =
+            int2Point ( l8, -l5 )
     in
     Canvas.group []
-    [   renderTentaclePixel env color (x, y+delta) l1 (rotate, addPoint offset offset1) flag1
-    ,   renderTentaclePixel env color (x, y+2*delta) l2 (rotate, addPoint offset offset2) flag2
-    ,   renderTentaclePixel env color (x, y-delta) l3 (rotate, addPoint offset offset3) flag3
-    ,   renderTentaclePixel env color (x, y-2*delta) l4 (rotate, addPoint offset offset4) flag4
-    ,   renderNum env rotate
-    ]
-    
-    
-renderTentaclePixel : EnvC -> Color -> Point -> Int -> (Int, Point) -> Int -> Renderable
-renderTentaclePixel env color pos l (rotate, offset) flag =
+        [ renderTentaclePixel env color ( x, y + delta ) l1 ( rotate, addPoint offset offset1 ) flag1
+        , renderTentaclePixel env color ( x, y + 2 * delta ) l2 ( rotate, addPoint offset offset2 ) flag2
+        , renderTentaclePixel env color ( x, y - delta ) l3 ( rotate, addPoint offset offset3 ) flag3
+        , renderTentaclePixel env color ( x, y - 2 * delta ) l4 ( rotate, addPoint offset offset4 ) flag4
+        , renderNum env rotate
+        ]
+
+
+renderTentaclePixel : EnvC -> Color -> Point -> Int -> ( Int, Point ) -> Int -> Renderable
+renderTentaclePixel env color pos l ( rotate, offset ) flag =
     --if (flag==1) then
-        shapes [ fill color ] [ rect (coorChange env (offsetPoint rotate offset pos)) (lengthChange env (toFloat l)) (lengthChange env (toFloat l)) ]
-    --else
-      --  Canvas.group [] []
+    shapes [ fill color ] [ rect (coorChange env (offsetPoint rotate offset pos)) (lengthChange env (toFloat l)) (lengthChange env (toFloat l)) ]
+
+
+
+--else
+--  Canvas.group [] []
+
 
 {-| applying offset & rotation to List Point for map function
-    rotate  direction (clock-wise degrees)
-    0       0
-    1       90
-    2       180
-    3       270
+rotate direction (clock-wise degrees)
+0 0
+1 90
+2 180
+3 270
 -}
 offsetPoint : Int -> Point -> Point -> Point
 offsetPoint rotate offset pos =
     let
-        (px, py) =  offset
-        (x, y) =    pos
-        (nx, ny) =  case  rotate of
-                        0 ->    (x, y)
-                        1 ->    (y, -x)
-                        2 ->    (-x, -y)
-                        3 ->    (-y, x)
-                        _ ->    (x, y)
-    in
-    (nx + px, ny + py)
+        ( px, py ) =
+            offset
 
-{-| generic function of rendering number(Int) for testing -}
+        ( x, y ) =
+            pos
+
+        ( nx, ny ) =
+            case rotate of
+                0 ->
+                    ( x, y )
+
+                1 ->
+                    ( y, -x )
+
+                2 ->
+                    ( -x, -y )
+
+                3 ->
+                    ( -y, x )
+
+                _ ->
+                    ( x, y )
+    in
+    ( nx + px, ny + py )
+
+
+{-| generic function of rendering number(Int) for testing
+-}
 renderNum : EnvC -> Int -> Renderable
 renderNum env num =
     text [ font { size = 24, family = "Arial", style = "" }, align Center ] (coorChange env ( 50, 50 )) ("rotate:" ++ String.fromInt num)
 
-{-| generic function of rendering circle -}
+
+{-| generic function of rendering circle
+-}
 renderCircle : EnvC -> Point -> Int -> Color -> Renderable
 renderCircle env pos radius color =
     shapes [ fill color ] [ circle (coorChange env pos) (lengthChange env (toFloat radius)) ]
 
-{-| render the enemy's core -}
+
+{-| render the enemy's core
+-}
 renderEnemyCore : EnvC -> Model -> Renderable
 renderEnemyCore env model =
     let
         ( locx, locy ) =
             int2Point model.core.loc
+
         ( x, y ) =
-            ( (locx+0.5)*cellLength, (locy+0.16)*cellLength )
+            ( (locx + 0.5) * cellLength, (locy + 0.16) * cellLength )
     in
     shapes
-        [   transform   
-                [   translate x y
-                ,   rotate ( degrees 45 )
-                ,   translate -x -y
-                ]
-        ,   fill Color.red
+        [ transform
+            [ translate x y
+            , rotate (degrees 45)
+            , translate -x -y
+            ]
+        , fill Color.red
         ]
-        [ rect ( coorChange env ( x, y ) ) ( lengthChange env (cellLength/2) ) ( lengthChange env (cellLength/2) ) ]
+        [ rect (coorChange env ( x, y )) (lengthChange env (cellLength / 2)) (lengthChange env (cellLength / 2)) ]
 
-{-| render the enmy's eye -}
+
+{-| render the enmy's eye
+-}
 renderEnemyEye : EnvC -> Model -> Renderable
 renderEnemyEye env model =
     let
-        eye = model.eye
+        eye =
+            model.eye
+
         pupil_offset =
             scalePointLength eye.v 8
+
         pupil_pos =
             addPoint eye.pos pupil_offset
     in
     Canvas.group
-    []
-    [
-        shapes [ fill Color.yellow ] [ circle ( coorChange env eye.pos) ( lengthChange env 20 )]
-    ,   shapes [ fill Color.red ] [ circle ( coorChange env pupil_pos) ( lengthChange env 15 ) ]
-    ]
+        []
+        [ shapes [ fill Color.yellow ] [ circle (coorChange env eye.pos) (lengthChange env 20) ]
+        , shapes [ fill Color.red ] [ circle (coorChange env pupil_pos) (lengthChange env 15) ]
+        ]
