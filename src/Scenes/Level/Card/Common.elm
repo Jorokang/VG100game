c6da8df6646918533e5e8dad641db698d29f8ff5
XuZifei <xuzifei20040316@sjtu.edu.cn>
2023-07-05 17:59:41 +0800

    bugFix: adjust to elm-format

--- src/Scenes/Level/Card/Common.elm
+++ src/Scenes/Level/Card/Common.elm
@@ -14,14 +14,21 @@ import Scenes.Level.LayerBase exposing (CommonData)
 Add your own data here.
 -}
 
+
+
 --card name and the cost
+
+
 type Card
     = CardName1 Int
     | N2 Int
     | N3 Int
 
 
+
 --take card from deck to hand, remove it to discard
+
+
 type alias Model =
     { hand : List Card
     , discard : List Card
@@ -33,9 +40,9 @@ type alias Model =
 -}
 nullModel : Model
 nullModel =
-    { hand = [CardName1 1]
+    { hand = [ CardName1 1 ]
     , discard = []
-    , deck = [N2 2, N3 4]
+    , deck = [ N2 2, N3 4 ]
     }
 
 
