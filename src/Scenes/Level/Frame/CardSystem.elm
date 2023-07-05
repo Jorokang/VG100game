1866bca0bc99c64c1172b30a3397a2e74c250c6b
XuZifei <xuzifei20040316@sjtu.edu.cn>
2023-07-05 18:47:34 +0800

    fix: move CardSystem to Frame level

--- src/Scenes/Level/Frame/CardSystem.elm
+++ src/Scenes/Level/Frame/CardSystem.elm
@@ -1,6 +1,8 @@
 module Scenes.Level.Card.CardSystem exposing (..)
 
 import Random exposing (Generator, Seed)
+import Scenes.Level.Card.CardUnique exposing (cardToEffect)
+import Tuple exposing (first)
 
 
 type alias Card =
@@ -71,13 +73,39 @@ drawCard model amount =
             ncard :: nmodel.hand
 
         nnmodel =
-            { nmodel | deck = ndeck, hand = nhand }
+            { nmodel | deck = sortPile ndeck, hand = nhand }
     in
     if amount == 1 then
         nnmodel
 
     else
         drawCard nnmodel (amount - 1)
+
+
+sortPile : List Card -> List Card
+sortPile pile =
+    List.sortBy .id pile
+
+
+dropCard : Model -> Int -> Model
+dropCard model pos =
+    let
+        ( dcard, nhand ) =
+            takeCard model.deck pos
+    in
+    { model | discard = dcard :: model.discard, hand = nhand }
+
+
+playCard : Model -> Int -> Model
+playCard model pos =
+    let
+        card =
+            first (takeCard model.hand pos)
+
+        nmodel =
+            dropCard model pos
+    in
+    cardToEffect nmodel card
 
 
 takeCard : List Card -> Int -> ( Card, List Card )
