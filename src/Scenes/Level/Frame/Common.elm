7f71baa01ca293912fca3db852d65f84d0673aa7
Wanglidi <lidiwang@sjtu.edu.cn>
2023-07-06 14:48:49 +0800

    feat: switch between player's and enemy's turn

--- src/Scenes/Level/Frame/Common.elm
+++ src/Scenes/Level/Frame/Common.elm
@@ -1,4 +1,7 @@
-module Scenes.Level.Frame.Common exposing (Model, EnvC, FrameStatus(..), nullModel, initFrame1)
+module Scenes.Level.Frame.Common exposing
+    ( Model, nullModel, EnvC
+    , FrameStatus(..), initFrame1
+    )
 
 {-| Common module
 
@@ -9,27 +12,27 @@ module Scenes.Level.Frame.Common exposing (Model, EnvC, FrameStatus(..), nullMod
 import Lib.Env.Env as Env
 import Scenes.Level.LayerBase exposing (CommonData)
 
+
 type FrameStatus
-    =   FramePlayerTurn
-    |   FrameEnemyTurn
-    |   FrameStopped
-    |   FrameInactive
+    = FramePlayerTurn
+    | FrameEnemyTurn
+    | FrameStopped
+    | FrameInactive
+
 
 {-| Model
 Add your own data here.
 -}
-
 type alias PlayerData =
-    {
-        cur_stamina : Int
-    ,   max_stamina : Int
+    { cur_stamina : Int
+    , max_stamina : Int
     }
 
+
 type alias Model =
-    {
-        status : FrameStatus
-    ,   time : Int
-    ,   player_data : PlayerData
+    { status : FrameStatus
+    , time : Int
+    , player_data : PlayerData
     }
 
 
@@ -37,23 +40,25 @@ type alias Model =
 -}
 nullModel : Model
 nullModel =
-    {
-        status = FrameInactive
-    ,   time = 0
-    ,   player_data =   {   cur_stamina = 0
-                        ,   max_stamina = 0
-                        }
+    { status = FrameInactive
+    , time = 0
+    , player_data =
+        { cur_stamina = 0
+        , max_stamina = 0
+        }
     }
+
 
 initFrame1 : Model
 initFrame1 =
-    {
-        status = FramePlayerTurn
-    ,   time = 0
-    ,   player_data =   {   cur_stamina = 3
-                        ,   max_stamina = 3
-                        }
+    { status = FramePlayerTurn
+    , time = 0
+    , player_data =
+        { cur_stamina = 3
+        , max_stamina = 3
+        }
     }
+
 
 {-| Convenient type alias for the environment
 -}
--- src/Scenes/Level/Frame/Render.elm
+++ src/Scenes/Level/Frame/Render.elm
@@ -1,1 +1,28 @@
 module Scenes.Level.Frame.Render exposing (..)
+
+import Canvas exposing (Point, Renderable, group, text)
+import Canvas.Settings exposing (fill)
+import Canvas.Settings.Text exposing (TextAlign(..), align, font)
+import Color exposing (Color)
+import Scenes.Level.Frame.Common exposing (EnvC, FrameStatus(..), Model)
+import Scenes.Level.Frame.Functions exposing (coorChange, nullCoorData)
+
+
+renderFrameStatus : EnvC -> Model -> Renderable
+renderFrameStatus env model =
+    let
+        str =
+            case model.status of
+                FramePlayerTurn ->
+                    "Player's turn"
+
+                FrameEnemyTurn ->
+                    "Enemy's turn"
+
+                FrameStopped ->
+                    "Stopped"
+
+                FrameInactive ->
+                    "Inactive"
+    in
+    text [ font { size = 48, family = "Arial", style = "" }, align Left ] (coorChange env ( 500, 500 ) nullCoorData) str
