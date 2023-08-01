0085daa2580098052d0c33311f904febac10f1e9
XuZifei <xuzifei20040316@sjtu.edu.cn>
2023-08-01 23:07:42 +0800

    doc: documentation for Level/Enemy

--- src/Scenes/Level/Grids/Model.elm
+++ src/Scenes/Level/Grids/Model.elm
@@ -90,6 +90,12 @@ updateModel env model =
             ( model, [], env )
 
 
+{-| updateModelRec
+Default update function
+
+Add your logic to handle LayerMsg here
+
+-}
 updateModelRec : EnvC -> LayerMsg -> Model -> ( Model, List ( LayerTarget, LayerMsg ), EnvC )
 updateModelRec env lmsg model =
     case lmsg of
@@ -112,6 +118,14 @@ updateModelRec env lmsg model =
             ( model, [], env )
 
 
+{-| viewModel
+Default view function
+
+If you don't have components, remove viewComponent.
+
+If you have other elements than components, add them after viewComponent.
+
+-}
 viewModel : EnvC -> Model -> Renderable
 viewModel env model =
     let
